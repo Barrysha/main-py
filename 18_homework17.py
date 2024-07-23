2158ffc1b9b03b306dd4a62cc14e1270ef90686f
Barrysha <metadonkis@gmail.com>
2024-07-23 07:17:04 +0300

    Update 18_homework17.py

--- 18_homework17.py
+++ 18_homework17.py
@@ -3,9 +3,9 @@ def single_root_words(root_word, *other_words):
 	same_words = []
 	
 	for word in other_words:
-		if root_word.lower() in word.lower():
+		if root_word.lower() in word.lower() or word.lower() in root_word.lower():
 			same_words.append(word)
 	
 	return same_words
 		
-print(single_root_words("Лес", "лесной", "лесничий", "лесничЕство", "карась"))
+print(single_root_words("Лес", "лесок", "лесной", "лесничЕство", "карась"))
